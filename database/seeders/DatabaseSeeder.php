6308d1dda6e297af51a22ca5280b5467fe7ef652
Tanveer Malik <46317722+CTSol186@users.noreply.github.com>
2022-03-26 18:43:05 +0500

    Added base files for laravel-roles Package

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RolesTableSeeder;
+use Illuminate\Database\Eloquent\Model;
+use Database\Seeders\PermissionsTableSeeder;
+use Database\Seeders\ConnectRelationshipsSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +19,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Model::unguard();
+
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(ConnectRelationshipsSeeder::class);
+        //$this->call('UsersTableSeeder');
+
+        Model::reguard();
     }
 }
